65264fa69fcba2d27a356eb74ff1188bb5bdeeec
a.fedorov035 <aleksandrnosov2008@gmail.com>
2025-09-12 16:45:23 +0300

    modified HomePage import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import Header from "../components/header/Header";
-import mainBanner from '../assets/images/main-banner.png';
+import mainBanner from "../assets/images/main-banner.png";
 import About from "../components/About/About";
 import HowItWorks from "../components/HowItWorks/HowItWorks";
-import Reviews from "../components/reviews/reviews";
+import Reviews from "../components/reviews/Reviews";
 import Footer from "../components/footer/Footer";
 
 export default function HomePage() {
   return (
     <div className="home">
-      <Header background={mainBanner} borderBottom='8px solid #FFA800'/>
+      <Header background={mainBanner} borderBottom="8px solid #FFA800" />
       <main>
         <About />
         <HowItWorks />
