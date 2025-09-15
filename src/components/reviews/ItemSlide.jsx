b54c74945ebb862b72d4643c035a3e794da010d0
a.fedorov035 <aleksandrnosov2008@gmail.com>
2025-09-15 18:57:58 +0300

    add public

--- src/components/reviews/ItemSlide.jsx
+++ src/components/reviews/ItemSlide.jsx
@@ -1,11 +1,16 @@
+const base = import.meta.env.BASE_URL;
+
 export default function ReviewItem({ element }) {
-    return (
-        <div className="review-item">
-            <img src={element.src} alt="image-review" />
-            <div className="review-content">
-                <span className='review-title'>{element.user}</span>
-                <p><span className='quotesLT'>“</span> {element.text} <span className='quotesRB'>”</span></p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="review-item">
+      <img src={base + element.src} alt="image-review" />
+      <div className="review-content">
+        <span className="review-title">{element.user}</span>
+        <p>
+          <span className="quotesLT">“</span> {element.text}{" "}
+          <span className="quotesRB">”</span>
+        </p>
+      </div>
+    </div>
+  );
 }
